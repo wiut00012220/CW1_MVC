96df95c0e5f01f64a16e076d79d115c93d6c10c9
wiut00012220 <wiut00012220@gmail.com>
2024-06-05 01:18:03 +0500

    first commit

---
+++ courseWork_MVC/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace courseWork_MVC.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public decimal Price { get; set; }
+
+    }
+}
